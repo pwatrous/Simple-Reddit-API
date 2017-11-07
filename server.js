5735c283780bc3ea1ab692e04affd8a46dfcd7fb
Patrick Watrous <patrickwatrous@live.com>
2017-11-07 01:05:37 -0500

    installed more dependencies and fixed bug. general skeleton completed

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 
 // set port
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-var router = express.Router();
-
-router.get("/", (req, res) => { 
+app.get("/", (req, res) => { 
 	res.json({ message: "Everything is working!" }); 
 });
 
-router.get("/subreddit/:subreddit_name", (req, res) => {
+app.get("/subreddit/:subreddit_name", (req, res) => {
 	res.json({ message: "This is where the json will be returned" });
-})
+});	
 
-
-app.listen(port);
-console.log('App listening on port: ' + port);
+app.listen(port, () => {
+	console.log('App listening on port: ' + port);
+});
