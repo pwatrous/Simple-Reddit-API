ac016f950b512db288c7689500976f692500c86e
Patrick Watrous <patrickwatrous@live.com>
2017-11-07 14:48:56 -0500

    added requests npm package and almost completed subreddit get request

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const app = express();
+const request = require("request");
 
 // set port
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => { 
-	res.json({ message: "Everything is working!" }); 
+	res.json({ success: "Everything is working!" }); 
 });
 
-app.get("/subreddit/:subreddit_name", (req, res) => {
-	res.json({ message: "This is where the json will be returned" });
+app.get("/subreddit/:name", (req, res) => {
+	let url = "http://www.reddit.com/r/" + req.params.name + ".json";
+	let json;
+	request(url, (error, response, body) => {
+		json = body;
+		console.log(json);
+		res.json({ data: "This is where the json will be returned" });
+	});
 });	
 
 app.listen(port, () => {
