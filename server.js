34108d8a9c8812502da6997c318075c3b325c5b6
Patrick Watrous <patrickwatrous@live.com>
2017-11-06 23:57:18 -0500

    basic skeleton for routes completed

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 
 router.get("/", (req, res) => { 
-	res.json({ message: 'hooray! welcome to our api!' }); 
+	res.json({ message: "Everything is working!" }); 
 });
+
+router.get("/subreddit/:subreddit_name", (req, res) => {
+	res.json({ message: "This is where the json will be returned" });
+})
 
 
 app.listen(port);
