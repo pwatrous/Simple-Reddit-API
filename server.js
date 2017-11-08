84a8492999a00353d0318ff7f307d96cbada1dcd
Patrick Watrous <patrickwatrous@live.com>
2017-11-08 02:32:58 -0500

    backend completed. I should probably go to bed now...

--- server.js
+++ server.js
@@ -13,8 +13,15 @@ app.get("/subreddit/:name", (req, res) => {
 	let url = "http://www.reddit.com/r/" + req.params.name + ".json";
 	request(url, function(error, response, body) {
 		let json = JSON.parse(body).data.children;
-		console.log(Array.isArray(json));
-		res.json({ data: json });
+		// fetches the subreddit name from the json response
+		let nameOfSubreddit = json[0].data.subreddit;
+		let subredditPosts = {};
+		for (let i = 0; i < json.length; i++) {
+			subredditPosts[i] = {};
+			subredditPosts[i].title = json[i].data.title;
+			subredditPosts[i].url = json[i].data.url;
+		}
+		res.json({ subreddit: nameOfSubreddit, posts: subredditPosts });
 	});
 });	
 
